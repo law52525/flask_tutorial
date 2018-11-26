c0d2502288ad1ee77eaf801a3e4bf5efa866be3b
law52525 <law52526@qq.com>
2018-11-26 11:46:57 +0800

    add blog Blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,7 +27,6 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
-    # app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
 
     @app.route('/hello')
     def hello():
